a930d14c0e22ea80356873d1e262ff932f9bac7d
Your Name <you@example.com>
2022-11-05 20:16:19 +0300

    05.11.2022 - 20:16:19,27

---
+++ testA/ExtractSound.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace testA
+{
+    internal class ExtractSound
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private string _enco;
+        private string _code;
+        public string Enco
+        {
+            get
+            {
+                return HttpUtility.UrlEncode(_enco);
+            }
+            set
+            {
+                _enco = value;
+            }
+        }
+        public string Code
+        {
+            get
+            {
+                return HttpUtility.UrlDecode(_code);
+            }
+            set
+            {
+                _code = value;
+            }
+        }
+        public async Task<string> GetCodeAsync(string str)
+        {
+            var values = new Dictionary<string, string>
+            {
+                { "f.req", "%5B%5B%5B%22jQ1olc%22%2C%22%5B%5C%22%E4%BF%84%E7%BD%97%E6%96%AF%5C%22%2C%5C%22zh-CN%5C%22%2Cnull%2C%5C%22undefined%5C%22%5D%22%2Cnull%2C%22generic%22%5D%5D%5D" },
+                { "at", "ADiessZ-V9qcF5UZLzb6X_0QfMn-%3A1667665235141" }
+            };
+            var URL = "https://translate.google.com/_/TranslateWebserverUi/data/batchexecute?rpcids=jQ1olc&source-path=/&f.sid=6148368410739897732&bl=boq_translate-webserver_20221102.06_p0&hl=ru&soc-app=1&soc-platform=1&soc-device=1&_reqid=1869635&rt=c";
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync(URL, content);
+
+            string responseString = await response.Content.ReadAsStringAsync();
+            return responseString;
+        }
+    }
+}
---
+++ testA/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace testA
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ExtractSound es = new ExtractSound();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Space)
+            {
+                es.Enco = tbIn.Text;
+                tbOut.Text = es.Enco;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //es.Enco = tbIn.Text;
+            //tbOut.Text = es.Enco;
+            tbOut.Text = es.GetCodeAsync("ee").Result;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            es.Code = tbIn.Text;
+            tbOut.Text = es.Code;
+        }
+    }
+}
